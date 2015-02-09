7fa51fe0e4b8da8ac21f35fa8e64b7313304a1d4
Billy Wolfington <man_of_magik903@yahoo.com>
2015-02-09 15:07:09 -0500

    Renamed Portal Start to Scene 0

--- Assets/Scripts/Portal_Horizontal.cs
+++ Assets/Scripts/Portal_Horizontal.cs
@@ -5,6 +5,7 @@ public class Portal_Horizontal : Portal {
 	
 	override
 	protected void SpawnExit() {
+		// TODO: Resolve spawning exiting inside of something
 		Vector3 exitLocation = transform.position;
 		exitLocation.x += positionOffset;
 		ExitDoor = Instantiate(ExitDoor, exitLocation, new Quaternion()) as GameObject;
--- Assets/Scripts/Portal_Vertical.cs
+++ Assets/Scripts/Portal_Vertical.cs
@@ -5,6 +5,7 @@ public class Portal_Vertical : Portal {
 
 	override
 	protected void SpawnExit() {
+		// TODO: Resolve spawning exiting inside of something
 		Vector3 exitLocation = transform.position;
 		exitLocation.y += positionOffset;
 		ExitDoor = Instantiate(ExitDoor, exitLocation, new Quaternion()) as GameObject;
