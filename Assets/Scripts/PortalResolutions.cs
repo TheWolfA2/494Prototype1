932be512ce20daa9c7d96f1e8fdbc834adafbd33
Billy Wolfington <man_of_magik903@yahoo.com>
2015-02-09 16:32:23 -0500

    Refactored the Environement prefab, added a script designed to resolve issues with portals inside of walls/floors

---
+++ Assets/Scripts/PortalResolutions.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalResolutions : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other) {
+		if (other.tag == "PortalEntrance" || other.tag == "PortalExit") {
+			print ("Portal entered!");
+		}
+	}
+
+	void OnTriggerStay(Collider other) {
+		OnTriggerEnter(other);
+	}
+	
+}
