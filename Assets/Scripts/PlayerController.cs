ecfe4622fbb321146ab3128a13a77995cca9b41e
Billy Wolfington <man_of_magik903@yahoo.com>
2015-02-09 14:23:00 -0500

    Added the door as a prefab

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,10 +10,13 @@ public class PlayerController : MonoBehaviour {
 
 	public float walkSpeed = 10f;
 	public float jumpSpeed = 8f;
+	public float portalBuffer = 0.5f;
 
 
 	// Use this for initialization
 	void Start () {
+		// Allow the user to generate portals immediately
+		currentTimeout = portalTimeout;
 	}
 
 	void FixedUpdate() {
@@ -25,13 +28,14 @@ public class PlayerController : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Walk(Input.GetAxis("Horizontal"));
-		Jump(Input.GetAxis("Vertical") > 0);
+		//Jump(Input.GetAxis("Vertical") > 0);
 		if (Input.GetButton("Fire1")) {
 			SummonPortal(true);
 		}
 		if (Input.GetButton("Fire2")) {
 			SummonPortal(false);
 		}
+		CheckForWall();
 	}
 
 	void Walk(float horizInput) {
@@ -48,6 +52,26 @@ public class PlayerController : MonoBehaviour {
 		newVel.y = jumpSpeed;
 		transform.rigidbody.velocity = newVel;
 	}
+
+	void CheckForWall() {
+		// Get the velocity
+		Vector3 horizontalMove = transform.rigidbody.velocity;
+		// Don't use the vertical velocity
+		horizontalMove.y = 0;
+		// Calculate the approximate distance that will be traversed
+		float distance =  horizontalMove.magnitude * Time.deltaTime;
+		// Normalize horizontalMove since it should be used to indicate direction
+		horizontalMove.Normalize();
+		RaycastHit hit;
+		
+		// Check if the body's current velocity will result in a collision
+		if(transform.rigidbody.SweepTest(horizontalMove, out hit, distance))
+		{
+			// If so, stop the movement
+			transform.rigidbody.velocity = new Vector3(0, transform.rigidbody.velocity.y, 0);
+		}
+	}
+
 
 	void SummonPortal(bool isVertical) {
 		if (currentTimeout >= portalTimeout) {
@@ -76,10 +100,22 @@ public class PlayerController : MonoBehaviour {
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "PortalEntrance") {
 			Teleport(other.GetComponent<Portal>());
+		} else if (other.tag == "Door") {
+//			print ("Entered a door!");
+			EnterDoor(other.GetComponent<LevelDoor>());
 		}
 	}
 
 	void Teleport(Portal p) {
 		p.EnterPortal(gameObject);
+
+		// Clear any movement player was experiencing before entering portal
+		Vector3 newVel = transform.rigidbody.velocity;
+		newVel.x = newVel.y = 0;
+		transform.rigidbody.velocity = newVel;
+	}
+
+	void EnterDoor(LevelDoor d) {
+		d.Open();
 	}
 }
