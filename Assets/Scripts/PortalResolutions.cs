02e8bf6a15608c42841cac1f2d4538a6ac1d66f7
Billy Wolfington <man_of_magik903@yahoo.com>
2015-02-09 23:27:20 -0500

    Added a basic Portal collision resolution scheme

--- Assets/Scripts/PortalResolutions.cs
+++ Assets/Scripts/PortalResolutions.cs
@@ -1,7 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+public enum PushDirection {
+	up,
+	down,
+	left,
+	right,
+}
+
 public class PortalResolutions : MonoBehaviour {
+
+	public PushDirection pushTo = PushDirection.up;
 
 	// Use this for initialization
 	void Start () {
@@ -15,12 +24,58 @@ public class PortalResolutions : MonoBehaviour {
 
 	void OnTriggerEnter(Collider other) {
 		if (other.tag == "PortalEntrance" || other.tag == "PortalExit") {
-			print ("Portal entered!");
+//			print ("Portal entered!");
+			ResolvePortalCollision(other);
 		}
 	}
 
 	void OnTriggerStay(Collider other) {
 		OnTriggerEnter(other);
 	}
-	
+
+	void ResolvePortalCollision(Collider other) {
+		// Case order: Up,Left,Down,Right
+		Vector3 newPortalPos = other.transform.position;
+
+		switch (pushTo) {
+		case PushDirection.up:
+			newPortalPos.y = CalculateNewY(other.transform.localScale.y, true);
+			break;
+		
+		case PushDirection.left:
+			newPortalPos.x = CalculateNewX(other.transform.localScale.x, false);
+			break;
+		
+		case PushDirection.down:
+			newPortalPos.y = CalculateNewY(other.transform.localScale.y, false);
+			break;
+		
+		case PushDirection.right:
+			newPortalPos.x = CalculateNewX(other.transform.localScale.x, true);
+			break;
+		}
+
+		other.transform.position = newPortalPos;
+	}
+
+	float CalculateNewX(float portalWidth, bool pushRight) {
+		float x = transform.position.x,
+			  offset = (transform.localScale.x / 2f + portalWidth / 2f);
+		if (pushRight) {
+			x += offset;
+		} else {
+			x -= offset;
+		}
+		return x;
+	}
+	float CalculateNewY(float portalHeight, bool pushUp) {
+		float y = transform.position.y,
+		      offset = (transform.localScale.y / 2f + portalHeight / 2f);
+		if (pushUp) {
+			y += offset;
+		} else {
+			y -= offset;
+		}
+		return y;
+	}
 }
